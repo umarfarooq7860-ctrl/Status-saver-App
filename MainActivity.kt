8756c160c1c8d0efb8f1e1e79758899592023fa1
umarfarooq7860-ctrl <umarfarooq12u2@gmail.com>
2026-07-17 22:46:53 -0700

    Add MainActivity with basic setup

---
+++ MainActivity.kt
@@ -0,0 +1,9 @@
+pakage com.statussaver.app 
+import android.os.Bundle
+import androidx.appcompat.appcompatActivity
+class MainActivity : AppcompatActivity () {
+  override fun onCreat (savedInstanceState: Bundle?) {
+    super .onCreat (saveInstanceState) 
+    setContentView(R.layout.activity_main)
+  }
+}
